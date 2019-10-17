13ae92084c6e5c6106fb7998f22dfb6a462b7f95
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-10-17 10:58:46 -0600

    still not working need to concatenate the ends somehow

---
+++ src/scripts/audrey.js
@@ -0,0 +1,21 @@
+const audrey = document.getElementById("audrey")
+
+/*
+    Add an event listener to the `document` object to listen
+    for the "scroll" event.
+*/
+audrey.addEventListener("scroll", function (event) {
+    audrey.style.width = window.scrollY/3px++;
+
+    /*
+        Adjust the width of audrey to be 1/3 the value of
+        `window.scrollY`. No lower than 50px, though.
+    */
+
+    audrey.style.height = window.scrollY/4px++;
+
+    /*
+        Adjust the height of audrey to be 1/4 the value of
+        `window.scrollY`. No lower than 100px, though.
+    */
+})
